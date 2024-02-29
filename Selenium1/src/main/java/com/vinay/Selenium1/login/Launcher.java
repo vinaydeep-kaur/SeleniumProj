2d9288ba6f510c43476169c8dbbbe242f826bafe
vinaydeepkaur <vinaydeepkaur8@gmail.com>
2024-02-29 14:36:35 +0530

    initial launch

--- Selenium1/src/main/java/com/vinay/Selenium1/login/Launcher.java
+++ Selenium1/src/main/java/com/vinay/Selenium1/login/Launcher.java
@@ -1,7 +1,15 @@
 package com.vinay.Selenium1.login;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Launcher {
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.google.com");
+		
+		String title = driver.getTitle();
+		System.out.println(title);
 	}
 }
