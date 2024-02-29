48cf5f002ed8651ff07c9d2a4b5d86c197ae45af
vinaydeep-kaur <kaurvinaydeep@gmail.com>
2024-02-29 14:49:06 +0530

    format

--- Selenium1/src/main/java/com/vinay/Selenium1/login/Launcher.java
+++ Selenium1/src/main/java/com/vinay/Selenium1/login/Launcher.java
@@ -6,10 +6,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Launcher {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
-		
-		driver.get("https://www.google.com");
-		
+
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
 		String title = driver.getTitle();
 		System.out.println(title);
+
 	}
 }
